624052dc776d058bb23b436a4fc8d61171babe6a
Ersin Dagli <41599748+ErsinDagli@users.noreply.github.com>
2018-08-18 12:01:43 +1000

    Updates to background images, recipes search list

---
+++ My Recipes/Recipe_App/Recipe_App/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Recipe_App.Views;
+using Xamarin.Forms;
+
+namespace Recipe_App.ViewModels
+{
+    class MainPageViewModel
+    {
+
+        public Command NavigateToAddRecipesPage => new Command(async () =>
+
+        await Application.Current.MainPage.Navigation.PushAsync(new AddRecipesPage()));
+
+
+
+        public Command NavigateToRecipeSearchPage => new Command(async () =>
+
+          await Application.Current.MainPage.Navigation.PushAsync(new TabbedPage1() {  BarBackgroundColor = Color.Black }));
+
+
+
+
+
+        
+
+        
+    }
+    
+}
